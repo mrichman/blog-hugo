1472d05f6b5a1391c90b54b948ca992cced676bd
Mark Richman <mark@markrichman.com>
2016-08-12 10:25:05 -0400

    Added README

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,17 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+# Try:
 # git submodule add -f -b master https://github.com/mrichman/mrichman.github.io.git public
+
+# $ rm -rf public/
+# git submodule add -f -b master https://github.com/mrichman/mrichman.github.io.git public
+# 'public' already exists in the index
+#
+# You have to unstage the rack directory first. Then you can add the submodule:
+#
+# $ git rm -r public
+# $ git submodule add -f -b master https://github.com/mrichman/mrichman.github.io.git public
 
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
