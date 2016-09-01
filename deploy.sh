2333dcfb7eba96ee381c48baeab190425348b2a2
Mark Richman <mark@markrichman.com>
2016-09-01 13:24:29 -0400

    rebuilding site Thu Sep  1 13:24:29 EDT 2016

--- deploy.sh
+++ deploy.sh
@@ -18,15 +18,22 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
 # Minify CSS
+# https://www.npmjs.com/package/minifier
 echo -e "\033[0;32mMinifying CSS...\033[0m"
-minify --no-comments --output ./public/css/main.min.css ./public/css/poole.css ./public/css/syntax.css ./public/css/hyde.css
+minify --no-comments --output ./public/css/main.min.css ./public/css/poole.css ./public/css/hyde.css
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
 echo -e "\033[0;32mCommitting changes to /public via git...\033[0m"
-# core.safecrlf turns off warning, not the function itself
-git config --global core.safecrlf false 
+
+# Windows workaround
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+	# core.safecrlf turns off warning, not the function itself
+	git config --global core.safecrlf false 
+fi
+
 git add -A
 
 # Commit changes.
